b5b87cb6b10ca79cd0b35a02aa306768cc4e4500
MarcoRianiUNIPR <mriani@unipr.it>
2022-07-26 16:13:28 +0200

    %% VARIANCE COMPARISON UNDER THE CONTAMINATION MODEL

---
+++ cap2/areVarComparison.m
@@ -0,0 +1,43 @@
+%% VARIANCE COMPARISON UNDER THE CONTAMINATION MODEL
+% Compute the variance of the sample mean and of the sample median (multiplied by 
+% n) under the contamination model $F_\epsilon$ (not all measurements are equally 
+% precise) 
+
+close all
+tau=(1:0.001:10)';
+lwd=3;
+FontSize=14;
+subplot(2,2,1)
+eps=0.05;
+nvarmean=(1-eps)+eps*(tau.^2);
+nvarMe=pi./(2*((1-eps +eps./tau).^2));
+hold('on')
+plot(tau,nvarmean,'r-','LineWidth',lwd)
+plot(tau,nvarMe,'b-.','LineWidth',lwd)
+xlabel('\tau','FontSize',FontSize)
+set(gca,'FontSize',FontSize)
+title('\epsilon=0.05','FontSize',FontSize)
+xlim([tau(1) tau(end)])
+ylim([1 11])
+
+subplot(2,2,2)
+eps=0.10;
+nvarmean1=(1-eps)+eps*(tau.^2);
+nvarMe1=pi./(2*((1-eps +eps./tau).^2));
+hold('on')
+plot(tau,nvarmean1,'r-','LineWidth',lwd)
+plot(tau,nvarMe1,'b-.','LineWidth',lwd)
+xlabel('\tau','FontSize',FontSize)
+set(gca,'FontSize',FontSize)
+title('\epsilon=0.10','FontSize',FontSize)
+xlim([tau(1) tau(end)])
+ylim([1 11])
+legend({'$n \times$ var($\overline y$)' '$n \times$ var(Me)'},'Interpreter','latex')
+vv=[tau,nvarmean nvarMe];
+vv1=[tau,nvarmean1 nvarMe1];
+
+prin=0;
+if prin==1
+    % print to postscript
+    print -depsc AREcont.eps;
+end
