c8c4ef52cca9657bf3d571eb15e8aca5f21e7b0f
MarcoRianiUNIPR <mriani@unipr.it>
2022-07-26 16:56:30 +0200

    IF for different estimators of location

---
+++ cap2/influence_function.m
@@ -0,0 +1,38 @@
+%% INFLUENCE FUNCTION at N(0,1)
+% Compute and show graphically the IF at N(0,1) for mean, median and trimmed 
+% mean in the interval -3, -2.9, ..., 3. Add legend and  appropriate titles for 
+% the axes
+
+
+% Show the IF at N(0,1) for mean, median and trimmed mean
+close all
+% Suppose mu=0;
+y=(-3:0.01:3)';
+% Compute the IF for the mean
+IFmean=y;
+% Compute the IF for the median
+IFmedian= sqrt(2*pi)*sign(y)/2;
+alpha=0.1;
+phiminus1oneminusalpha=norminv(1-alpha);
+% Compute the IF for the trimmed mean
+IFyalpha=zeros(length(y),1);
+for i=1:length(y)
+    IFyalpha(i)=(sign(y(i))/(1-2*alpha))*min([abs(y(i)); phiminus1oneminusalpha]);
+end
+% Plotting part
+
+LineWidth=2;
+FontSize=16;
+hold('on')
+plot(y,IFmean,'Color','r','LineStyle','-','LineWidth',LineWidth)
+plot(y,IFmedian,'Color','b','LineStyle','-.','LineWidth',LineWidth)
+plot(y,IFyalpha,'Color','k','LineStyle','--','LineWidth',LineWidth)
+legend({'IF$_{\overline y}$' 'IF$_{\mbox{Me}}$' 'IF$_{\overline y_{0.1}}$'},'FontSize',20,'Location','southeast','Interpreter','latex')
+xlabel('$\it y$','FontSize',FontSize,'Interpreter','latex')
+ylabel('IF','FontSize',FontSize,'Interpreter','latex')
+
+prin=0;
+if prin==1
+    % print to postscript
+    print -depsc IF.eps;
+end
