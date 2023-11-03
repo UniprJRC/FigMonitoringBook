c59af418c8b442c19e81c18d1b04a0f231214ddf
MarcoRianiUNIPR <mriani@unipr.it>
2023-11-03 16:47:37 +0100

    Removed trimmeanFS.m

--- cap2/trimmeanFS.m
+++
@@ -1,9 +0,0 @@
-function meantrimmed = trimmeanFS(y,alpha)
-%trimmeanFS trims a proportion alpha of observations from both ends
-n=length(y);
-ysor=sort(y);
-m=floor((n-1)*alpha);
-meantrimmed=mean(ysor(m+1:n-m));
-end
-
-
