9e16a19c366314c18740b80d212c6ce55bc8d5ba
MarcoRianiUNIPR <mriani@unipr.it>
2023-11-03 09:48:30 +0100

    Improved files for Income2

--- cap10/Income2regression.m
+++ cap10/Income2regression.m
@@ -1,7 +1,8 @@
 %% This file is referred to dataset Income2
-% Creates all Figures and tables associated with dataset Income2
-% See Table A.2 in Appendix A to see where this dataset has been used
-% inside the book
+% Regression analysis of dataset Income2 
+% It creates Figures 10.12 ---- 10.21
+% and Tables 10.2 ----- 10.4
+
 
 %% Data loading
 clear
@@ -20,179 +21,6 @@ one=ones(n,1);
 % prin = 
 prin=0;
 
-%% Analysis of the score test
-% Table 1.3
-la=[-2  -1.5 -1 -0.5 0 1];
-out=Score(y,one, 'la',la);
-disp([la' out.Score])
-
-Score=out.Score;
-rownam=["-2" "-1.5" "Inverse" "Reciprocal square root" "Logarithmic" "None"];
-colnam=["lambda" "Score test"];
-
-ScoreT=array2table([la' Score],"RowNames",rownam,"VariableNames",colnam);
-format bank
-disp('Table 1.3')
-disp(ScoreT)
-
-%% Compute statistics in the original and transformed scale
-% Table 1.4 with lambda=1, lambda=-1 or lambda=-1.5
-
-ysor=sort(y);
-
-alpha=0.10;
-m=floor((n-1)*alpha);
-meanTri=mean(ysor(m+1:n-m));
-mea=mean(y);
-medi=median(y);
-sta=std(y);
-consfact=1/norminv(0.75);
-madn=consfact*mad(y,1);
-
-loc=[mea; meanTri; medi; sta; madn];
-yori=y;
-
-
-laj=-1;
-y1=100000*(yori.^laj);
-
-ysor=sort(y1);
-alpha=0.10;
-m=floor((n-1)*alpha);
-meanTri1=mean(ysor(m+1:n-m));
-mea1=mean(y1);
-medi1=median(y1);
-sta1=std(y1);
-madn1=consfact*mad(y1,1);
-
-loc1=[mea1; meanTri1; medi1; sta1; madn1];
-
-
-laj=-1.5;
-y1=10000000*(yori.^laj);
-ysor=sort(y1);
-meanTri1=mean(ysor(m+1:n-m));
-mea1=mean(y1);
-medi1=median(y1);
-sta1=std(y1);
-madn1=consfact*mad(y1,1);
-
-loc2=[mea1; meanTri1; medi1; sta1; madn1];
-
-LOC=[loc loc1 loc2];
-
-rn=["Mean" "Trimmed mean" "Median" "Standard Deviation" "MADN"];
-vn=["Original data" "Inverse transf" "la=-1.5"];
-LOCt=array2table(LOC,'RowNames',rn,'VariableNames',vn);
-format bank
-disp(LOCt)
-
-
-%% Create Figure 1.6 fanplot
-% Fanplot using just the intercept
-outFSRfanUNI=FSRfan(y,one,'intercept',0,'la',[-2 -1.5 -1 -0.5],'tag','fanplotnoExpl');
-title('Figure 1.6')
-if prin==1
-    % print to postscript
-    print -depsc fanIncome2.eps;
-end
-
-
-%% Analysis of the trimmed mean (Prepare input for Figure 
-% Exercise 1.4 - part (a)
-
-alphaAll=(0:0.01:0.5)';
-lalphaAll=length(alphaAll);
-meanTru=zeros(lalphaAll,1);
-% meanTru1=meanTru;
-ysor=sort(y);
-for i=1:lalphaAll
-    m=floor((n-1)*alphaAll(i));
-    meanTru(i)=mean(ysor(m+1:n-m));
-    % meanTru1(i)=trimmean(y,100*alphaAll(i));
-
-end
-
-
-%% Create figure A.1 
-% Exercise 1.4 - part (b)
-figure
-FontSize=14;
-plot(alphaAll,meanTru)
-xlabel('\alpha','FontSize',FontSize)
-xlim([0 0.5])
-meany=mean(y);
-mediany=median(y);
-yline(meany)
-yline(mediany)
-ylabel('$\overline y_\alpha$','Interpreter','latex','FontSize',FontSize)
-text(0.45,meany-60,"$\hat \mu = \overline y_n$",'Interpreter','latex','FontSize',FontSize)
-text(0.15,mediany+60,"Me",'Interpreter','latex','FontSize',FontSize)
-
-set(gca,"XDir","reverse")
-set(gcf,"Name",'Figure A.1')
-title('Figure A.1')
-prin=0;
-if prin==1
-    % print to postscript
-    print -depsc trimmeanIncome2.eps;
-end
-
-
-
-%% Create figure A.2.  
-% Exercise 1.4 - part (c)
-figure
-yl1=-0.8;
-yl2=0.1;
-yrs=y/max(y);
-subplot(2,3,1)
-ytra=normBoxCox(yrs,1,1,'Jacobian',true);
-boxplot(ytra,'Labels',{''})
-title('$\lambda=1$','Interpreter','latex','FontSize',FontSize)
-ylim([yl1 yl2])
-
-
-subplot(2,3,2)
-ytra=normBoxCox(yrs,1,0,'Jacobian',true);
-boxplot(ytra,'Labels',{''})
-title('$\lambda=0$','Interpreter','latex','FontSize',FontSize)
-ylim([yl1 yl2])
-
-subplot(2,3,3)
-ytra=normBoxCox(yrs,1,-0.5,'Jacobian',true);
-boxplot(ytra,'Labels',{''})
-title('$\lambda=-0.5$','Interpreter','latex','FontSize',FontSize)
-ylim([yl1 yl2])
-
-subplot(2,3,4)
-ytra=normBoxCox(yrs,1,-1,'Jacobian',true);
-boxplot(ytra,'Labels',{''})
-title('$\lambda=-1$','Interpreter','latex','FontSize',FontSize)
-ylim([yl1 yl2])
-
-subplot(2,3,5)
-ytra=normBoxCox(yrs,1,-1.5,'Jacobian',true);
-boxplot(ytra,'Labels',{''})
-title('$\lambda=-1.5$','Interpreter','latex','FontSize',FontSize)
-ylim([yl1 yl2])
-
-subplot(2,3,6)
-ytra=normBoxCox(yrs,1,-2,'Jacobian',true);
-boxplot(ytra,'Labels',{''})
-title('$\lambda=-2$','Interpreter','latex','FontSize',FontSize)
-ylim([yl1 yl2])
-sgtitle('Figure A.2')
-set(gcf,"Name",'Figure A.2')
-
-prin=0;
-if prin==1
-    % print to postscript
-    print -depsc boxlaIncome2.eps;
-end
-
-%  BEGIN OF REGRESSION ANALYSIS (CHAPTER 10)
-
 %% Figure 10.12 yXplot
 yXplot(yt,Xt);
 sgtitle("Figure 10.12")
@@ -200,6 +28,8 @@ if prin==1
     % print to postscript
     print -depsc inc2f1.eps;
 end
+
+
 
 %% Fit in the original scale using all the observations
 disp("Table 10.2")
@@ -346,7 +176,6 @@ if prin==1
     print -depsc inc2f7bot.eps;
 end
 
-
 %% Create Table 10.3
 disp("Table 10.3")
 disp('ANOVA in the transformed scale using all obs')
@@ -436,8 +265,4 @@ format short
 mdlAVAtra=fitlm(outrobAV.tX,outrobAV.ty,'Exclude',outrobAV.outliers);
 disp('Table 10.4')
 disp(mdlAVAtra)
-
-
-
-
 
