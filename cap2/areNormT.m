e94c88b39ac752cc62049d944a02ab25f4b527b7
MarcoRianiUNIPR <mriani@unipr.it>
2023-11-03 11:03:30 +0100

    arenormT updated

--- cap2/areNormT.m
+++ cap2/areNormT.m
@@ -1,17 +1,17 @@
-%% Compute empirical and theoretical ARE(mean median)
-% For N(0,1) and then for Student T with 5 degrees of freedomm.
-% Then use Student T with 2, 2.1, 2.2, ..., 10 degrees of
-% Plot the value of the ARE(mean,median) empirical and theoretical
+%% Compute empirical and theoretical ARE(Me, Mean) 
+close all
 
-%% ARE curve as function of degrees of freedom
+%% Theoretical ARE(Me, Mean) when the data are from the Normal distribution
 % Var(sample mean)/Var(sample median)
-    varmedian=1/(2*normpdf(0))^2;
-    varmean=1;
-    VarRatio=varmean/varmedian;
+varmedian=1/(2*normpdf(0))^2;
+varmean=1;
+VarRatio=varmean/varmedian;
+disp(['Theoretical ARE(Me,mean) under the normal distribution='  num2str(VarRatio)] )
 
-    %% Compare ARE(mean,median) under the normal distribution
+%% Empirical ARE(Me, Mean) when the data are from the Normal distribution
+% This section is not mentioned in the book
 n=10000000;
-nsimul=10000;
+nsimul=100; % Increare nsimul if you like greater precision
 meandistr=zeros(nsimul,1);
 mediandistr=zeros(nsimul,1);
 
@@ -21,28 +21,16 @@ parfor j=1:nsimul
     mediandistr(j)=median(y);
 end
 
-disp('ARE(mean,median) empirical when N(0.1)')
+disp('Empirical ARE(Me, Mean) under the normal distribution=')
+disp([num2str(nsimul) ' simulations using n=' num2str(n)])
 disp(var(meandistr)/var(mediandistr))
 
 
-%% Compare ARE(mean,median) under the normal distribution
-n=100000;
-nsimul=10000;
-meandistr=zeros(nsimul,1);
-mediandistr=zeros(nsimul,1);
 
-parfor j=1:nsimul
-    y=randn(n,1);
-    meandistr(j)=mean(y);
-    mediandistr(j)=median(y);
-end
-
-disp('ARE(mean,median) empirical when N(0.1)')
-disp(var(meandistr)/var(mediandistr))
-
-%% Compare ARE(mean,median) under the Student t_5
+%% Empirical ARE(Me,mean) under the Student t_5
+% This section is not mentioned in the book
 n=100;
-nsimul=10000;
+nsimul=1000;
 meandistr=zeros(nsimul,1);
 mediandistr=zeros(nsimul,1);
 % nu = degrees of freedom of Student T
@@ -54,11 +42,11 @@ for j=1:nsimul
     mediandistr(j)=median(y);
 end
 
-disp('ARE(mean,median) empirical with T(5)')
+disp(['Empirical ARE(Me, Mean) under t' num2str(nu)])
+disp([num2str(nsimul) ' simulations using n=' num2str(n)])
 disp(var(meandistr)/var(mediandistr))
 
-%% Compute the ARE as function of degrees of freedom
-% Var(sample mean)/Var(sample median)
+%% Compute Empirical ARE as function of degrees of freedom
 n=100;
 nsimul=50000;
 nuall=2.1:0.1:10;
@@ -82,19 +70,18 @@ for nu=nuall
 end
 
 %% Plot the empirical ARE  as function of degrees of freedom of Student T
-% Var(sample mean)/Var(sample median)
-close all
 ld=1.2;
 fs=16;
 plot(nuall,smooth(VarRatioE),'LineWidth',ld)
 hline=refline(0,1);
 hline.Color = 'r';
 xlabel('Degrees of freedom of Student $t$','FontSize',fs-2,'Interpreter','latex')
-ylabel('ARE(Me,$\overline y$)','Interpreter','latex','FontSize',fs)
+ylabel('Empirical ARE(Me,$\overline y$)','Interpreter','latex','FontSize',fs)
+title('Figure not given in the book')
 
 
-
-%% Plot the  theoretical ARE curve as function of degrees of freedom
+%% Create input for Figure 2.3 
+% Theoretical ARE curve as function of degrees of freedom
 % Var(sample mean)/Var(sample median)
 % nu = degrees of freedom of Student T
 ij=0;
@@ -105,10 +92,10 @@ for nu=nuall
     VarRatioT(ij)=varmean/varmedian;
 end
 
-
+%% Create Figure 2.3
 ld=1.2;
 fs=16;
-close all
+figure
 hold('on')
 % plot(nuall,(VarRatioE),'r','LineWidth',ld)
 plot(nuall,VarRatioT,'b', 'LineWidth',ld)
@@ -117,6 +104,17 @@ hline=refline(0,1);
 hline.Color = 'r';
 xlabel('Degrees of freedom of Student $t$','FontSize',fs-2,'Interpreter','latex')
 ylabel('ARE(Me,$\overline y$)','Interpreter','latex','FontSize',fs)
+title('Figure 2.3')
+set(gcf,"Name",'Figure 2.3')
+
+%% ARE for selected degrees of freedom
+nusel=3:5; % degrees of freedom selected
+[~,posnusel]=intersect(nuall,nusel);
+VarRatioTsel=flip(VarRatioT(posnusel));
+rownam="t_"+nusel+" ARE(Me,mean)";
+VarRatioTselt=array2table(VarRatioTsel',"RowNames",rownam, ...
+    "VariableNames","ARE for selected degrees of freedom");
+disp(VarRatioTselt)
 
 prin=0;
 if prin==1
