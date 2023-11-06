72c94bea3cc940cc86d42d3d72eb97c839d895c1
MarcoRianiUNIPR <mriani@unipr.it>
2023-11-06 15:21:15 +0100

    Hawkins data material

--- cap4/Hawkins.m
+++ cap4/Hawkins.m
@@ -1,62 +1,75 @@
-%% HD
+%% hawkins data
+% This file creates Figure 4.23 
+% and Figures 4.27-4.29
+close all;
 load('hawkins.txt');
 y=hawkins(:,9);
 X=hawkins(:,1:8);
 
-yXplot(y,X)
+n=length(y);
 prin=0;
+
+%% Create Figure 4.23
+% yX plot
+yXplot(y,X)
+sgtitle('Figure 4.23')
+set(gcf,"Name",'Figure 4.23')
 if prin==1
     % print to postscript
     print -depsc HDyXplot.eps;
 end
 
-%%  HD: resfwdplot persistent brushing with FS
-% Interactive_example
-
-[outLXS]=LXS(y,X,'nsamp',10000);
-[out]=FSReda(y,X,outLXS.bs);
-%%
-databrush=struct;
-databrush.bivarfit='';
-databrush.selectionmode='Brush'; % Brush selection
-databrush.persist='on'; % Enable repeated mouse selections
-databrush.Label='off'; % Write labels of trajectories while selecting
-databrush.RemoveLabels='off'; % Do not remove labels after selection
-% Note that if you wish to have the labels for the brushed units in the
-% yXplot it is necessary to add the instruction in the line below
-% databrush.labeladd='1'
-mdrplot(out,'ylimy',[1 8])
-resfwdplot(out,'databrush',databrush,'datatooltip','');
+%% Create Figure 4.27
+out=FSR(y,X,'plots',0);
+seq=1:n;
+goodOBS=setdiff(seq,out.ListOut);
+yXplot(y(goodOBS),X(goodOBS,:),'tag','pl_goodobs')
+sgtitle('Figure 4.27')
+set(gcf,"Name",'Figure 4.27')
 
 if prin==1
     % print to postscript
-    print -depsc HD2a.eps;
-    print -depsc HD2b.eps;
-
-        print -depsc HD3a.eps;
-    print -depsc HD3b.eps;
-
-        print -depsc HD4a.eps;
-    print -depsc HD4b.eps;
-
+    print -depsc HDyXplotgood.eps;
 end
-%% Sregeda with PD link
-out1=Sregeda(y,X,'plots',1,'rhofunc','mdpd');
+
+
+%% Prepare input for Figures 4.28 and 4.29
+% Sregeda with PD link
+outPD=Sregeda(y,X,'plots',0,'rhofunc','mdpd');
+outTB=Sregeda(y,X,'plots',1,'rhofunc','bisquare');
+
+%% Create Figure 28 left panel
+resfwdplot(outTB,'tag','pl_TBori');
+title('Figure 4.28 (left panel)')
+set(gcf,"Name",'Figure 4.28 (left panel)')
+
+%% Create Figure 28 right panel
+resfwdplot(outTB,'tag','pl_PDori');
+title('Figure 4.28 (right panel)')
+set(gcf,"Name",'Figure 4.28 (right panel)')
+
+%% Create Figure 29 left panel
+resfwdplot(outTB,'tag','pl_TBzoom');
+title('Figure 4.29 (left panel)')
+set(gcf,"Name",'Figure 4.28 (left panel)')
 yl=5;
 ylim([-yl yl])
-if prin==1
-    % print to postscript
-    print -depsc HDpdlink.eps;
-    print -depsc HDpdlinkZ.eps;
-end
 
-%% Sregeda with bisquare link
-outTB=Sregeda(y,X,'plots',1,'rhofunc','bisquare');
+%% Create Figure 29 right panel
+resfwdplot(outTB,'tag','pl_PDzoom');
+title('Figure 4.29 (right panel)')
+set(gcf,"Name",'Figure 4.29 (right panel)')
+yl=5;
 ylim([-yl yl])
+
 if prin==1
     % print to postscript
     print -depsc HDtblink.eps;
     print -depsc HDtblinkZ.eps;
+
+    print -depsc HDpdlink.eps;
+    print -depsc HDpdlinkZ.eps;
+
 end
 
 
@@ -64,72 +77,57 @@ end
 
 %% FS brushing
 %
-
-out=FSReda(y,X);
-ylim([-10 10])
-% After click on the legend to show just good units
-if prin==1
-    % print to postscript
-    print -depsc HDyXplotgood.eps;
-end
-%
-
-%% FSReda with brushing
-clearvars;close all;
-load('hawkins.txt');
-y=hawkins(:,9);
-X=hawkins(:,1:8);
-yXplot(y,X);
-[outLXS]=LXS(y,X,'nsamp',10000);
-[out]=FSReda(y,X,outLXS.bs);
-
-yXplot(out,'selstep',[40 21 80],'selunit','40000',...
-    'databrush',{'persist','on','selectionmode' 'Brush'});
-databrush=1;
-resfwdplot(out,'databrush',databrush)
-
-
-
-
-%% Sregeda with bisquare link brushing
-% For the exercises
-% Perform botht the FS and the S residuals monitoring
-clearvars;close all;
-load('hawkins.txt');
-y=hawkins(:,9);
-X=hawkins(:,1:8);
-yXplot(y,X);
-[outLXS]=LXS(y,X,'nsamp',10000);
-[out]=FSReda(y,X,outLXS.bs);
-
-outTB=Sregeda(y,X,'plots',1,'rhofunc','bisquare');
-yl=5;
-ylim([-yl yl])
-% The information about the order of entry of the units in the FS is added
-% to outTB
-outTB.Un=out.Un;
-
-%%
-mdrplot(out,'ylimy',[1 8])
-
-standard=struct;
-standard.ylim=[-yl yl];
-
-databrush=struct;
-databrush.bivarfit='';
-databrush.selectionmode='Brush'; % Brush selection
-% databrush.persist='on'; % Enable repeated mouse selections
-databrush.Label='off'; % Write labels of trajectories while selecting
-databrush.RemoveLabels='off'; % Do not remove labels after selection
-
-resfwdplot(outTB,'databrush',databrush,'datatooltip','','standard',standard)
-if prin==1
-    % print to postscript
-    print -depsc HDtbexeres6.eps;
-    print -depsc HDtbexemdr6.eps;
-
-    print -depsc HDtbexeres.eps;
-    print -depsc HDtbexemdr.eps;
-    print -depsc HDtbexeyXplot.eps;
-    
-end
+% 
+% out=FSReda(y,X);
+% ylim([-10 10])
+% % After click on the legend to show just good units
+% if prin==1
+%     % print to postscript
+%     print -depsc HDyXplotgood.eps;
+% end
+% %
+% 
+% 
+% 
+% %% Sregeda with bisquare link brushing
+% % For the exercises
+% % Perform botht the FS and the S residuals monitoring
+% clearvars;close all;
+% load('hawkins.txt');
+% y=hawkins(:,9);
+% X=hawkins(:,1:8);
+% yXplot(y,X);
+% [outLXS]=LXS(y,X,'nsamp',10000);
+% [out]=FSReda(y,X,outLXS.bs);
+% 
+% outTB=Sregeda(y,X,'plots',1,'rhofunc','bisquare');
+% yl=5;
+% ylim([-yl yl])
+% % The information about the order of entry of the units in the FS is added
+% % to outTB
+% outTB.Un=out.Un;
+% 
+% %%
+% mdrplot(out,'ylimy',[1 8])
+% 
+% standard=struct;
+% standard.ylim=[-yl yl];
+% 
+% databrush=struct;
+% databrush.bivarfit='';
+% databrush.selectionmode='Brush'; % Brush selection
+% % databrush.persist='on'; % Enable repeated mouse selections
+% databrush.Label='off'; % Write labels of trajectories while selecting
+% databrush.RemoveLabels='off'; % Do not remove labels after selection
+% 
+% resfwdplot(outTB,'databrush',databrush,'datatooltip','','standard',standard)
+% if prin==1
+%     % print to postscript
+%     print -depsc HDtbexeres6.eps;
+%     print -depsc HDtbexemdr6.eps;
+% 
+%     print -depsc HDtbexeres.eps;
+%     print -depsc HDtbexemdr.eps;
+%     print -depsc HDtbexeyXplot.eps;
+% 
+% end
--- cap4/HawkinsInteractive.m
+++ cap4/HawkinsInteractive.m
@@ -1,4 +1,7 @@
-%% HD
+%% Hawkins data
+% This file creates Figures 4.24-4.26
+% Note that this file needs interactivity
+
 load('hawkins.txt');
 y=hawkins(:,9);
 X=hawkins(:,1:8);
@@ -10,7 +13,6 @@ prin=0;
 
 [outLXS]=LXS(y,X,'nsamp',10000);
 [out]=FSReda(y,X,outLXS.bs);
-%%
 databrush=struct;
 databrush.bivarfit='';
 databrush.selectionmode='Brush'; % Brush selection
@@ -22,16 +24,21 @@ databrush.RemoveLabels='off'; % Do not remove labels after selection
 % databrush.labeladd='1'
 mdrplot(out,'ylimy',[1 8])
 resfwdplot(out,'databrush',databrush,'datatooltip','');
+fig=findobj(0,'tag','pl_mdr');
+figure(fig)
+title('Figure similar to bottom panel of 4.24 or 4.25 or 4.26','It depends on your brushing')
+fig=findobj(0,'tag','pl_resfwd');
+figure(fig)
+title('Figure similar to top panel of 4.24 or 4.25 or 4.26', 'It depends on your brushing')
 
 if prin==1
     % print to postscript
     print -depsc HD2a.eps;
     print -depsc HD2b.eps;
 
-        print -depsc HD3a.eps;
+    print -depsc HD3a.eps;
     print -depsc HD3b.eps;
 
-        print -depsc HD4a.eps;
+    print -depsc HD4a.eps;
     print -depsc HD4b.eps;
-
 end
